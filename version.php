ec97acdcd49c99a7a07a0d92143b9e710ad7d577
Daniel Neis Araujo <danielneis@gmail.com>
2021-03-08 16:35:36 -0300

    bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_lpprogress';
-$plugin->release = '2';
-$plugin->version = 2021030300;
+$plugin->release = '3';
+$plugin->version = 2021030800;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
